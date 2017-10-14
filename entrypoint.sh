0cf4a055b01cee3009aefc746fdcb337d3453b19
Willem Hulshof <w.hulshof@magnus.nl>
2017-10-14 14:43:40 +0200

    $ALLOW_LIST

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 while true
 do
         # wait until file is modified
-        inotifywait -e modify  /php/code/KeyPortal/iplog-www
+        inotifywait -e modify  $ALLOW_LIST
 
 
                 echo "copy Nginx allow-list"
-                cp /php/code/KeyPortal/iplog-www /etc/nginx/conf.d/keyportal-allow.conf
+                cp $ALLOW_LIST /etc/nginx/conf.d/keyportal-allow.conf
                 echo "Reloading Nginx Configuration"
                 curl --unix-socket /var/run/docker.sock -X POST http:/v1.24/containers/nginx/kill?signal=HUP
 
